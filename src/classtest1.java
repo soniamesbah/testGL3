10a9cd97be59172f99cca7844d8ee5e07354f792
Sonya <Sonya@Sonya-PC.mshome.net>
2014-02-15 10:48:36 +0100

    pfffffffffff 3ème test

---
+++ src/classtest1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Sonya
+ */
+public class classtest1 {
+    
+    private num int;
+    
+    }
+    
+    
+    
+
